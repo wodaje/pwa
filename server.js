9f98b60bd68abf21f67c0ec3081736fca7701a97
Jeff Woda <wodahouse@gmail.com>
2021-03-24 22:13:51 -0400

    depploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/offline", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
